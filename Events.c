cd2a49c07cbfeed4c7cd1e8811712ad22a31eb16
Eneias Júnior <37703932+jrprett@users.noreply.github.com>
2021-07-18 00:04:18 -0300

    Add files via upload

--- Events.c
+++ Events.c
@@ -1,11 +1,11 @@
 /* ###################################################################
 **     Filename    : Events.c
-**     Project     : Roteiro1_v.2
+**     Project     : Roteiro1_IoT
 **     Processor   : MKL25Z128VLK4
 **     Component   : Events
 **     Version     : Driver 01.00
 **     Compiler    : GNU C Compiler
-**     Date/Time   : 2019-04-03, 13:04, # CodeGen: 0
+**     Date/Time   : 2019-03-14, 09:41, # CodeGen: 0
 **     Abstract    :
 **         This is user's event module.
 **         Put your event handler code here.
@@ -53,6 +53,172 @@ extern "C" {
 void Cpu_OnNMIINT(void)
 {
   /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  Uart2_ESP_OnError (module Events)
+**
+**     Component   :  Uart2_ESP [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart2_ESP_OnError(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  Uart2_ESP_OnRxChar (module Events)
+**
+**     Component   :  Uart2_ESP [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart2_ESP_OnRxChar(void)
+{
+  /* Write your code here ... */
+  extern volatile char ESP_receive;
+  ESP_receive = 1;
+}
+
+/*
+** ===================================================================
+**     Event       :  Uart2_ESP_OnTxChar (module Events)
+**
+**     Component   :  Uart2_ESP [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart2_ESP_OnTxChar(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  Uart0_OpenSDA_OnError (module Events)
+**
+**     Component   :  Uart0_OpenSDA [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart0_OpenSDA_OnError(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  Uart0_OpenSDA_OnRxChar (module Events)
+**
+**     Component   :  Uart0_OpenSDA [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart0_OpenSDA_OnRxChar(void)
+{
+  /* Write your code here ... */
+  extern volatile char OPEN_SDA_receive;
+  OPEN_SDA_receive = 1;
+}
+
+/*
+** ===================================================================
+**     Event       :  Uart0_OpenSDA_OnTxChar (module Events)
+**
+**     Component   :  Uart0_OpenSDA [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart0_OpenSDA_OnTxChar(void)
+{
+  /* Write your code here ... */
+  
+}
+
+
+/*
+** ===================================================================
+**     Event       :  RTC1_OnSecond (module Events)
+**
+**     Component   :  RTC1 [RTC_LDD]
+*/
+/*!
+**     @brief
+**         Called each second if OnSecond event is enabled (see
+**         [SetEventMask] and [GetEventMask] methods) and RTC device is
+**         enabled. This event is available only if [Interrupt
+**         service/event] is enabled.
+**     @param
+**         UserDataPtr     - Pointer to the user or
+**                           RTOS specific data. This pointer is passed
+**                           as the parameter of Init method. 
+*/
+/* ===================================================================*/
+void RTC1_OnSecond(LDD_TUserData *UserDataPtr)
+{
+  /* Write your code here ... */
+  extern volatile char TimeStatus;
+  TimeStatus = 1;
+}
+
+/*
+** ===================================================================
+**     Event       :  PWM1_OnEnd (module Events)
+**
+**     Component   :  PWM1 [PWM]
+**     Description :
+**         This event is called when the specified number of cycles has
+**         been generated. (Only when the component is enabled -
+**         <Enable> and the events are enabled - <EnableEvent>). The
+**         event is available only when the <Interrupt service/event>
+**         property is enabled and selected peripheral supports
+**         appropriate interrupt.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void PWM1_OnEnd(void)
+{
+  /* Write your code here ... */
+  extern volatile char PWM;
+  PWM = 1;
 }
 
 /* END Events */
--- Events.h
+++ Events.h
@@ -1,11 +1,11 @@
 /* ###################################################################
 **     Filename    : Events.h
-**     Project     : Roteiro1_v.2
+**     Project     : Roteiro1_IoT
 **     Processor   : MKL25Z128VLK4
 **     Component   : Events
 **     Version     : Driver 01.00
 **     Compiler    : GNU C Compiler
-**     Date/Time   : 2019-04-03, 13:04, # CodeGen: 0
+**     Date/Time   : 2019-03-14, 09:41, # CodeGen: 0
 **     Abstract    :
 **         This is user's event module.
 **         Put your event handler code here.
@@ -34,6 +34,22 @@
 #include "PE_Error.h"
 #include "PE_Const.h"
 #include "IO_Map.h"
+#include "Uart0_OpenSDA.h"
+#include "ASerialLdd1.h"
+#include "Uart2_ESP.h"
+#include "ASerialLdd2.h"
+#include "UTIL1.h"
+#include "MCUC1.h"
+#include "TempSensor.h"
+#include "AdcLdd1.h"
+#include "RTC1.h"
+#include "TU1.h"
+#include "PWM1.h"
+#include "PwmLdd1.h"
+#include "Bit1.h"
+#include "BitIoLdd1.h"
+#include "Bit2.h"
+#include "BitIoLdd2.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -54,6 +70,138 @@ extern "C" {
 /* ===================================================================*/
 void Cpu_OnNMIINT(void);
 
+
+/*
+** ===================================================================
+**     Event       :  Uart2_ESP_OnError (module Events)
+**
+**     Component   :  Uart2_ESP [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart2_ESP_OnError(void);
+
+/*
+** ===================================================================
+**     Event       :  Uart2_ESP_OnRxChar (module Events)
+**
+**     Component   :  Uart2_ESP [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart2_ESP_OnRxChar(void);
+
+/*
+** ===================================================================
+**     Event       :  Uart2_ESP_OnTxChar (module Events)
+**
+**     Component   :  Uart2_ESP [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart2_ESP_OnTxChar(void);
+
+/*
+** ===================================================================
+**     Event       :  Uart0_OpenSDA_OnError (module Events)
+**
+**     Component   :  Uart0_OpenSDA [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart0_OpenSDA_OnError(void);
+
+/*
+** ===================================================================
+**     Event       :  Uart0_OpenSDA_OnRxChar (module Events)
+**
+**     Component   :  Uart0_OpenSDA [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart0_OpenSDA_OnRxChar(void);
+
+/*
+** ===================================================================
+**     Event       :  Uart0_OpenSDA_OnTxChar (module Events)
+**
+**     Component   :  Uart0_OpenSDA [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Uart0_OpenSDA_OnTxChar(void);
+
+/*
+** ===================================================================
+**     Event       :  RTC1_OnSecond (module Events)
+**
+**     Component   :  RTC1 [RTC_LDD]
+*/
+/*!
+**     @brief
+**         Called each second if OnSecond event is enabled (see
+**         [SetEventMask] and [GetEventMask] methods) and RTC device is
+**         enabled. This event is available only if [Interrupt
+**         service/event] is enabled.
+**     @param
+**         UserDataPtr     - Pointer to the user or
+**                           RTOS specific data. This pointer is passed
+**                           as the parameter of Init method. 
+*/
+/* ===================================================================*/
+void RTC1_OnSecond(LDD_TUserData *UserDataPtr);
+
+/*
+** ===================================================================
+**     Event       :  PWM1_OnEnd (module Events)
+**
+**     Component   :  PWM1 [PWM]
+**     Description :
+**         This event is called when the specified number of cycles has
+**         been generated. (Only when the component is enabled -
+**         <Enable> and the events are enabled - <EnableEvent>). The
+**         event is available only when the <Interrupt service/event>
+**         property is enabled and selected peripheral supports
+**         appropriate interrupt.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void PWM1_OnEnd(void);
 
 /* END Events */
 
